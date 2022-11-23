2a53d96f7cd74c9162960422f82def805083443a
mkthaw14 <mt859948@gmail.com>
2022-11-23 13:03:58 +0630

    Third Commit

--- src/main/java/com/mmit/model/repos/OrderRepo.java
+++ src/main/java/com/mmit/model/repos/OrderRepo.java
@@ -13,16 +13,16 @@ import com.mmit.projection.MonthlyOrderSale;
 
 public interface OrderRepo extends JpaRepository<Orders, Long> {
 	
-	@Query(value = "SELECT Date(created_at) as order_date, sum(amount) as daily_sales FROM `orders` Where status != 'cancelled' GROUP By Date(created_at) ORDER By Date(created_at) DESC LIMIT 5", nativeQuery = true)
+	@Query(value = "SELECT Date(created_at) as order_date, sum(amount) as daily_sales FROM orders Where status != 'cancelled' GROUP By Date(created_at) ORDER By Date(created_at) DESC LIMIT 5", nativeQuery = true)
 	List<DailyOrderSale> findDailyOrderSalesInLastFiveDays();
 	
-	@Query(value = "SELECT year(created_at) as year, month(created_at) as month, sum(amount) as monthly_sales From orders Where status != 'cancelled' GROUP By year(created_at), month(created_at) Order By year(created_at), month(created_at) DESC LIMIT 1", nativeQuery = true)
+	@Query(value = "SELECT extract(year from created_at) as year, extract(month from created_at) as month,  sum(amount) as monthly_sales From orders Where status != 'cancelled' GROUP By extract(year from created_at), extract(month from created_at) Order By extract(year from created_at), extract(month from created_at) DESC LIMIT 1", nativeQuery = true)
 	MonthlyOrderSale findLatestMonthOrderSales();
 
 	@Query(value = "SELECT COUNT(*) FROM orders  WHERE status = 'pending'", nativeQuery = true)
 	long findPendingOrderCount();
 	
-	@Query(value = "SELECT * FROM `orders` WHERE status != 'received' And status != 'cancelled' And customer_id = :customerID", nativeQuery = true)
+	@Query(value = "SELECT * FROM orders WHERE status != 'received' And status != 'cancelled' And customer_id = :customerID", nativeQuery = true)
 	List<Orders> findOrderByPendingAndDeliveredStatus(@Param("customerID") int customerID);
 
 	@Query("SELECT o FROM Orders o WHERE o.customer.id = :id")
